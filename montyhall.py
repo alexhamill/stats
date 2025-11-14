1128554c6feee048183dc673a36017ba87fdf2d2
Alex Hamill <alexander.hamill@icloud.com>
2025-11-14 11:16:36 -0500

    Monty Hall Problem ( look I codded somthing Andrew)

---
+++ montyhall.py
@@ -0,0 +1,22 @@
+sportscar = 0
+goat = 0
+repeats = 0
+
+while repeats < 100000000:
+    import random
+
+    doors = ['goat', 'goat', 'sportscar']
+    random.shuffle(doors)
+
+    selection = random.randint(0, 2)
+
+    if doors[selection] == 'sportscar':
+        goat += 1
+    else:
+        sportscar += 1
+    repeats += 1
+
+print("Sports Car:",sportscar)
+print("Goat:",goat)
+print("Total:",repeats)
+print("Winning Percentage:",(sportscar/repeats)*100,"%")
