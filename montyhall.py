0a6f7b72af0891de89d69e2cb850c5f247030073
Alex Hamill <alexander.hamill@icloud.com>
2025-11-15 13:26:15 -0500

    chaning amout of doors

--- montyhall.py
+++ montyhall.py
@@ -1,23 +1,31 @@
 import random
 
+Iterations = 10000000
+doors = 1000
+removes = 99
+
+n = doors
+if removes > n - 2:
+    raise ValueError("removes must be <= number of other doors - 1")
+
 sportscar = 0
 goat = 0
-repeats = 0
 
-while repeats < 100000000:
-
-    doors = ['goat', 'goat', 'sportscar']
-    random.shuffle(doors)
-
-    selection = random.randint(0, 2)
-
-    if doors[selection] == 'sportscar':
-        goat += 1
+for _ in range(Iterations):
+    car = random.randrange(n)     
+    sel = random.randrange(n)     
+    
+    if car != sel:
+        if random.randrange(n - removes - 1) == 0:
+            sportscar += 1
+        else:
+            goat += 1
     else:
-        sportscar += 1
-    repeats += 1
+        goat += 1
 
-print("Sports Car:",sportscar)
-print("Goat:",goat)
-print("Total:",repeats)
-print("Winning Percentage:",(sportscar/repeats)*100,"%")
+# goat = Iterations - sportscar
+
+print("Sports Car:", sportscar)
+print("Goat:", goat)
+print("Total:", Iterations)
+print("Winning Percentage:", (sportscar / Iterations) * 100, "%")
